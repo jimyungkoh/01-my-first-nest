9e513c86ccbce0d528885eaf6c32495c3a023e9f
jimyungkoh <kojimyung1206@naver.com>
2024-02-20 17:18:26 +0900

    fix: productcontrollers '/' and ':productId' path

--- src/domains/products/products.controller.ts
+++ src/domains/products/products.controller.ts
@@ -6,12 +6,12 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  async findAll() {
+    return await this.productsService.findAll();
   }
 
   @Get(':productId')
-  findOne(@Param('productId') productId: string) {
-    return this.productsService.findOne(+productId);
+  async findOne(@Param('productId') productId: string) {
+    return await this.productsService.findOne(+productId);
   }
 }
