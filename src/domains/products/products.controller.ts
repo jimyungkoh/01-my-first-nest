23db6315a124451837f1c3c77cc02be4d7051269
jimyungkoh <kojimyung1206@naver.com>
2024-02-20 17:14:37 +0900

    feat: productcontrollers '/' and ':productId' path

--- src/domains/products/products.controller.ts
+++ src/domains/products/products.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':productId')
+  findOne(@Param('productId') productId: string) {
+    return this.productsService.findOne(+productId);
+  }
 }
