ee999c1312296a5616058f13160fe3aa5fbdfa3e
jimyungkoh <kojimyung1206@naver.com>
2024-02-20 17:09:22 +0900

    fix: 없는 property 선언 제거

--- src/domains/products/products.controller.ts
+++ src/domains/products/products.controller.ts
@@ -1,42 +1,7 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { Controller } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
-
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return;
-  }
-
-  @Get()
-  findAll() {
-    return this.productsService.findAll();
-  }
-
-  @Get(':productId')
-  findOne(@Param('productId') productId: string) {
-    return this.productsService.findOne(+productId);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return;
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return;
-  }
 }
