f9954a1aa63d6c62eb5e34599f3bf23d1c4ec7de
jimyungkoh <kojimyung1206@naver.com>
2024-02-21 17:31:29 +0900

    feat: 미들웨어 사용 선언, 브랜드 도메인 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './database/prisma/prisma.module';
@@ -8,6 +9,9 @@ import { UserProfileModule } from './domains/accounts/users/user-profile/user-pr
 import { CartsModule } from './domains/carts/carts.module';
 import { OrdersModule } from './domains/orders/orders.module';
 import { ProductsModule } from './domains/products/products.module';
+import { SignedInOnlyGuard } from './guards/signed-in-only.guard';
+import { AuthMiddleware } from './middlewares/auth.middleware';
+import { BrandsModule } from './domains/brands/brands.module';
 
 @Module({
   // 모듈에 필요한 다른 모듈들을 가져옵니다.
@@ -22,6 +26,7 @@ import { ProductsModule } from './domains/products/products.module';
     CartsModule,
     UserProfileModule,
     PrismaModule,
+    BrandsModule,
   ],
 
   // 이 모듈에서 사용할 컨트롤러를 정의합니다.
@@ -29,10 +34,14 @@ import { ProductsModule } from './domains/products/products.module';
 
   // 이 모듈에서 사용할 서비스(Provider)를 정의합니다.
   // => 의존성을 주입해주는 부분(Controllers에 AppService 넣어줘)
-  providers: [AppService],
+  providers: [AppService, { useClass: SignedInOnlyGuard, provide: APP_GUARD }],
 
   // exports: [],
   // exports 배열은 이 모듈에서 제공하고 다른 모듈에서 사용할 수 있게 하는 provider들의 목록입니다.
   // 다른 모듈에서 이 모듈을 import할 때, exports에 정의된 provider만이 외부에서 접근 가능합니다.
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cors from 'cors';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/http.exception.filter';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<INestApplication>(AppModule);
+  app.use(cors());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
